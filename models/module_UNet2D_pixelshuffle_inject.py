b8dded2df2cf86611bf91cc5d7fffbefea1ac84a
Dennis Eschweiler <dennis.eschweiler@rwth-aachen.de>
2023-11-10 10:47:20 +0100

    Removed redundant function definition

--- models/module_UNet2D_pixelshuffle_inject.py
+++ models/module_UNet2D_pixelshuffle_inject.py
@@ -8,21 +8,7 @@ import math
 import torch
 import torch.nn as nn
 
-
-# code from https://huggingface.co/blog/annotated-diffusion
-class SinusoidalPositionEmbeddings(nn.Module):
-    def __init__(self, dim):
-        super().__init__()
-        self.dim = dim
-
-    def forward(self, t):
-        device = t.device
-        half_dim = self.dim // 2
-        embeddings = math.log(10000) / (half_dim - 1)
-        embeddings = torch.exp(torch.arange(half_dim, device=device) * -embeddings)
-        embeddings = t[:, None] * embeddings[None, :]
-        embeddings = torch.cat((embeddings.sin(), embeddings.cos()), dim=-1)
-        return embeddings
+from ThirdParty.diffusion import SinusoidalPositionEmbeddings
     
     
 
